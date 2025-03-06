5afa3a43fbb83df843ab5b1c2b763929a9cf5c6c
chiyizi <ch1y1z1@qq.com>
2025-03-06 17:48:19 +0800

    fix use rho when viz callback

--- layers.py
+++ layers.py
@@ -90,6 +90,9 @@ if __name__ == "__main__":
                 )
         
         if idx % save_load_config["viz_freq"] == 0:
+            rhos = rhos.reshape((len(layers), -1))
+            for idx in range(num_layers):
+                layers[idx].use_rho(rhos[idx])
             os.makedirs(f"{save_load_config['save_path']}/{t}/epochs_{idx}", exist_ok=True)
             for ndx in range(num_masks):
                 fig, ax = plt.subplots(1, num_layers, figsize=(4 * num_layers, 4))
